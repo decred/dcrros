59e513b2e9408d73e51beb0874424ca2bc4880aa
Matheus Degiovani <opensource@matheusd.com>
2020-10-30 14:35:38 -0300

    backend: Add chain preprocessing tests
    This adds tests to assert the behavior of chain preprocessing, which
    generates the underlying data used for the Account service.

---
+++ backend/common_test.go
@@ -0,0 +1,353 @@
+package backend
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"decred.org/dcrros/backend/backenddb"
+	"decred.org/dcrros/backend/internal/badgerdb"
+	"decred.org/dcrros/backend/internal/memdb"
+	"github.com/decred/dcrd/chaincfg/chainhash"
+	"github.com/decred/dcrd/chaincfg/v3"
+	"github.com/decred/dcrd/dcrjson/v3"
+	"github.com/decred/dcrd/dcrutil/v3"
+	chainjson "github.com/decred/dcrd/rpc/jsonrpc/types/v2"
+	"github.com/decred/dcrd/wire"
+	"github.com/stretchr/testify/require"
+)
+
+// defaultTimeout is the default timeout for test contexts.
+const defaultTimeout = time.Second * 30
+
+// testCtx returns a context that gets canceled after defaultTimeout or after
+// the test ends.
+func testCtx(t *testing.T) context.Context {
+	ctxt, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	t.Cleanup(cancel)
+	return ctxt
+}
+
+// testDbInstances executes the specified test on all available DB
+// implementations.
+func testDbInstances(t *testing.T, parallel bool, test func(t *testing.T, db backenddb.DB)) {
+	testCases := []struct {
+		name  string
+		newDb func(t *testing.T) backenddb.DB
+	}{{
+		name: "memdb",
+		newDb: func(t *testing.T) backenddb.DB {
+			db, err := memdb.NewMemDB()
+			require.NoError(t, err)
+			return db
+		},
+	}, {
+		name: "badger-mem",
+		newDb: func(t *testing.T) backenddb.DB {
+			db, err := badgerdb.NewBadgerDB("")
+			require.NoError(t, err)
+			return db
+		},
+	}, {
+		name: "badger",
+		newDb: func(t *testing.T) backenddb.DB {
+			tmpDir, err := ioutil.TempDir("", "dcrros_badger")
+			require.NoError(t, err)
+			t.Cleanup(func() { os.RemoveAll(tmpDir) })
+			db, err := badgerdb.NewBadgerDB(tmpDir)
+			require.NoError(t, err)
+			return db
+		},
+	}}
+
+	for _, tc := range testCases {
+		ok := t.Run(tc.name, func(t *testing.T) {
+			if parallel {
+				t.Parallel()
+			}
+			db := tc.newDb(t)
+			t.Cleanup(func() {
+				err := db.Close()
+				if err != nil {
+					t.Logf("Error closing db: %v", err)
+				}
+			})
+			test(t, db)
+		})
+		if !ok {
+			break
+		}
+	}
+}
+
+// mustHex decodes the given string as a byte slice. It must only be used with
+// hardcoded values.
+func mustHex(s string) []byte {
+	b, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+// mockChain is a mock struct used for tests, which fulfills the chain
+// interface.
+//
+// Note that this chain does _not_ follow all consensus rules since it doesn't
+// have an underlying blockchain instance guaranteeing its correct behavior.
+// Tests are responsible for creating an appropriate chain, according to the
+// specific assertions they will perform.
+//
+// The exported functions are performed under the mtx lock. Unexported
+// functions are not and callers need to lock the chain if performing
+// concurrent modifications.
+type mockChain struct {
+	t      *testing.T
+	noncer *rand.Rand
+
+	mtx            sync.Mutex
+	blocks         map[chainhash.Hash]*wire.MsgBlock
+	blocksByHeight map[int64]*chainhash.Hash
+	tipHash        chainhash.Hash
+	tipHeight      int64
+	txByHash       map[chainhash.Hash]*wire.MsgTx
+
+	// The following *hook functions are called by the corresponding
+	// exported functions if they are defined.
+
+	getBlockHook     func(ctx context.Context, blockHash *chainhash.Hash) (*wire.MsgBlock, error)
+	getBlockHashHook func(ctx context.Context, blockHeight int64) (*chainhash.Hash, error)
+}
+
+type blockMangler func(b *wire.MsgBlock)
+
+func newMockChain(t *testing.T, params *chaincfg.Params) *mockChain {
+	m := &mockChain{
+		t:              t,
+		noncer:         rand.New(rand.NewSource(0xfafef1f0f00000)),
+		blocks:         make(map[chainhash.Hash]*wire.MsgBlock),
+		blocksByHeight: make(map[int64]*chainhash.Hash),
+		txByHash:       make(map[chainhash.Hash]*wire.MsgTx),
+		tipHash:        params.GenesisHash,
+	}
+
+	// Add genesis.
+	m.blocks[params.GenesisHash] = params.GenesisBlock
+	m.blocksByHeight[0] = &params.GenesisHash
+
+	return m
+}
+
+func (mc *mockChain) addCoinbase(b *wire.MsgBlock) {
+	pksCb := mustHex("76a914ca2adec002d8fd0992b169ae7a4779076e150c2d88ac")
+
+	// Add a random nonce to ensure each coinbase is unique.
+	var opRet [8]byte
+	mc.noncer.Read(opRet[:])
+	tx := &wire.MsgTx{
+		TxIn: []*wire.TxIn{
+			{}, // Coinbase
+		},
+		TxOut: []*wire.TxOut{
+			{PkScript: pksCb},    // Treasury
+			{PkScript: opRet[:]}, // OP_RETURN
+			{PkScript: pksCb},    // PoW reward
+		},
+	}
+	b.Transactions = append(b.Transactions, tx)
+}
+
+// addSudoTx adds a pseudo-tx directly to the tx index, allowing this tx to be
+// used as source for another tx. The given value and pks are used in the
+// single tx out.
+func (mc *mockChain) addSudoTx(value int64, pkScript []byte) chainhash.Hash {
+	// Add a random nonce to ensure each tx is unique.
+	var prevHash [32]byte
+	mc.noncer.Read(prevHash[:])
+
+	tx := wire.NewMsgTx()
+	tx.AddTxIn(wire.NewTxIn(&wire.OutPoint{Hash: prevHash}, 0, nil))
+	tx.AddTxOut(wire.NewTxOut(value, pkScript))
+	txh := tx.TxHash()
+	mc.txByHash[txh] = tx
+	return txh
+}
+
+// txToAddr returns a block mangler that adds a transaction that creates
+// outputs paying to a specific pkScript.
+//
+// `nb` outputs are created in the tx, each paying `value` amount.
+//
+// Note the source tx for the funds is only added to tx index for input
+// fetching and _not_ to the block itself.
+func (mc *mockChain) txToAddr(pkScript []byte, value, nb int64) blockMangler {
+	return func(b *wire.MsgBlock) {
+		prevHash := mc.addSudoTx(0, []byte{})
+		tx := wire.NewMsgTx()
+		prevOut := &wire.OutPoint{Hash: prevHash}
+		tx.AddTxIn(wire.NewTxIn(prevOut, value*nb, nil))
+		for i := int64(0); i < nb; i++ {
+			tx.AddTxOut(wire.NewTxOut(value, pkScript))
+		}
+		b.Transactions = append(b.Transactions, tx)
+	}
+}
+
+// fromAddr returns a block mangler that creates a tx with `nb` inputs spending
+// `value` coins from pseudo-tx outputs that use the given `pkScript`.
+//
+// Note the source outputs spent are _not_ added to the block, only to the tx
+// index to allow input fetching.
+func (mc *mockChain) txFromAddr(pkScript []byte, value, nb int64) blockMangler {
+	return func(b *wire.MsgBlock) {
+		tx := wire.NewMsgTx()
+		for i := int64(0); i < nb; i++ {
+			prevHash := mc.addSudoTx(value, pkScript)
+			prevOut := &wire.OutPoint{Hash: prevHash}
+			tx.AddTxIn(wire.NewTxIn(prevOut, value, nil))
+		}
+		tx.AddTxOut(wire.NewTxOut(0, []byte{}))
+		b.Transactions = append(b.Transactions, tx)
+	}
+}
+
+// extendTip generates a new block with a coinbase extending the current tip,
+// calls every passed mangler and finally adds the block to the blockchain,
+// replacing the current tip with it.
+func (mc *mockChain) extendTip(manglers ...blockMangler) {
+	// Basic block extending tip.
+	height := mc.tipHeight + 1
+	hdr := wire.BlockHeader{
+		Height:    uint32(height),
+		PrevBlock: mc.tipHash,
+		Nonce:     uint32(mc.noncer.Int31()),
+		VoteBits:  1, // Approves parent.
+	}
+	b := wire.NewMsgBlock(&hdr)
+	mc.addCoinbase(b)
+
+	// Call manglers.
+	for _, f := range manglers {
+		f(b)
+	}
+
+	// Update tx index.
+	for _, tx := range b.Transactions {
+		txh := tx.TxHash()
+		mc.txByHash[txh] = tx
+	}
+	for _, tx := range b.STransactions {
+		txh := tx.TxHash()
+		mc.txByHash[txh] = tx
+	}
+
+	// Update tip.
+	bh := b.BlockHash()
+	mc.blocks[bh] = b
+	mc.blocksByHeight[height] = &bh
+	mc.tipHeight = height
+	mc.tipHash = bh
+}
+
+// rewindChain rewinds the chain tip the given number of blocks.
+//
+// Note this doesn't currently clean up blocks that have been rewinded-out.
+func (mc *mockChain) rewindChain(nbBlocks int) {
+	mc.t.Helper()
+
+	height := mc.tipHeight - int64(nbBlocks)
+	bh := mc.blocksByHeight[height]
+	if bh == nil {
+		mc.t.Fatalf("unable to rewind chaing %d blocks from tip %d",
+			nbBlocks, mc.tipHeight)
+	}
+
+	mc.tipHeight = height
+	mc.tipHash = *bh
+}
+
+func (mc *mockChain) GetBlockChainInfo(ctx context.Context) (*chainjson.GetBlockChainInfoResult, error) {
+	return nil, nil
+}
+
+func (mc *mockChain) getBlockHash(ctx context.Context, blockHeight int64) (*chainhash.Hash, error) {
+	if blockHeight > mc.tipHeight {
+		return nil, &dcrjson.RPCError{Code: dcrjson.ErrRPCOutOfRange}
+	}
+	return mc.blocksByHeight[blockHeight], nil
+}
+
+func (mc *mockChain) GetBlockHash(ctx context.Context, blockHeight int64) (*chainhash.Hash, error) {
+	mc.mtx.Lock()
+	defer mc.mtx.Unlock()
+
+	if mc.getBlockHashHook != nil {
+		return mc.getBlockHashHook(ctx, blockHeight)
+	}
+	return mc.getBlockHash(ctx, blockHeight)
+}
+
+func (mc *mockChain) getBlock(ctx context.Context, blockHash *chainhash.Hash) (*wire.MsgBlock, error) {
+	bl, ok := mc.blocks[*blockHash]
+	if !ok {
+		return nil, &dcrjson.RPCError{Code: dcrjson.ErrRPCBlockNotFound}
+	}
+
+	return bl, nil
+}
+
+func (mc *mockChain) GetBlock(ctx context.Context, blockHash *chainhash.Hash) (*wire.MsgBlock, error) {
+	mc.mtx.Lock()
+	defer mc.mtx.Unlock()
+
+	if mc.getBlockHook != nil {
+		return mc.getBlockHook(ctx, blockHash)
+	}
+
+	return mc.getBlock(ctx, blockHash)
+}
+
+func (mc *mockChain) GetRawTransaction(ctx context.Context, txHash *chainhash.Hash) (*dcrutil.Tx, error) {
+	mc.mtx.Lock()
+	defer mc.mtx.Unlock()
+
+	tx, ok := mc.txByHash[*txHash]
+	if !ok {
+		return nil, fmt.Errorf("tx does not exist")
+	}
+	return dcrutil.NewTx(tx), nil
+}
+
+func (mc *mockChain) GetBestBlockHash(ctx context.Context) (*chainhash.Hash, error) {
+	return nil, nil
+}
+
+func (mc *mockChain) GetRawMempool(ctx context.Context, txType chainjson.GetRawMempoolTxTypeCmd) ([]*chainhash.Hash, error) {
+	return nil, nil
+}
+
+func (mc *mockChain) SendRawTransaction(ctx context.Context, tx *wire.MsgTx, allowHighFees bool) (*chainhash.Hash, error) {
+	return nil, nil
+}
+
+func (mc *mockChain) NotifyBlocks(ctx context.Context) error {
+	return nil
+}
+
+func (mc *mockChain) GetInfo(ctx context.Context) (*chainjson.InfoChainResult, error) {
+	return nil, nil
+}
+
+func (mc *mockChain) Version(ctx context.Context) (map[string]chainjson.VersionResult, error) {
+	return nil, nil
+}
+
+func (mc *mockChain) Connect(ctx context.Context, retry bool) error {
+	return nil
+}
---
+++ backend/svc_account_test.go
@@ -0,0 +1,479 @@
+package backend
+
+import (
+	"context"
+	"strconv"
+	"testing"
+	"time"
+
+	"decred.org/dcrros/backend/backenddb"
+	rtypes "github.com/coinbase/rosetta-sdk-go/types"
+	"github.com/decred/dcrd/chaincfg/chainhash"
+	"github.com/decred/dcrd/chaincfg/v3"
+	"github.com/decred/dcrd/wire"
+	"github.com/stretchr/testify/require"
+)
+
+func accountBalanceByHeight(t *testing.T, svr *Server, height int64,
+	account string) int64 {
+	t.Helper()
+
+	req := &rtypes.AccountBalanceRequest{
+		BlockIdentifier: &rtypes.PartialBlockIdentifier{
+			Index: &height,
+		},
+		AccountIdentifier: &rtypes.AccountIdentifier{
+			Address: account,
+			Metadata: map[string]interface{}{
+				"script_version": uint16(0),
+			},
+		},
+	}
+	res, rerr := svr.AccountBalance(testCtx(t), req)
+	require.Nil(t, rerr)
+	gotBalance, err := strconv.ParseInt(res.Balances[0].Value, 10, 64)
+	require.NoError(t, err)
+	return gotBalance
+}
+
+func accountBalanceByHash(t *testing.T, svr *Server, hash chainhash.Hash,
+	account string) int64 {
+	t.Helper()
+
+	bh := hash.String()
+	req := &rtypes.AccountBalanceRequest{
+		BlockIdentifier: &rtypes.PartialBlockIdentifier{
+			Hash: &bh,
+		},
+		AccountIdentifier: &rtypes.AccountIdentifier{
+			Address: account,
+			Metadata: map[string]interface{}{
+				"script_version": uint16(0),
+			},
+		},
+	}
+	res, rerr := svr.AccountBalance(testCtx(t), req)
+	require.Nil(t, rerr)
+	gotBalance, err := strconv.ParseInt(res.Balances[0].Value, 10, 64)
+	require.NoError(t, err)
+	return gotBalance
+}
+
+func accountBalanceAtTip(mc *mockChain, svr *Server, account string) int64 {
+	mc.t.Helper()
+
+	req := &rtypes.AccountBalanceRequest{
+		AccountIdentifier: &rtypes.AccountIdentifier{
+			Address: account,
+			Metadata: map[string]interface{}{
+				"script_version": uint16(0),
+			},
+		},
+	}
+	res, rerr := svr.AccountBalance(testCtx(mc.t), req)
+	require.Nil(mc.t, rerr)
+
+	// Ensure the balance was returned for the current chain tip.
+	if mc.tipHeight != res.BlockIdentifier.Index {
+		mc.t.Fatalf("block index returned by AccountBalance() does not "+
+			"match chain tip. want=%d got=%d", mc.tipHeight,
+			res.BlockIdentifier.Index)
+	}
+	if mc.tipHash.String() != res.BlockIdentifier.Hash {
+		mc.t.Fatalf("block hash returned by AccountBalance() does not " +
+			"match chain tip.")
+	}
+
+	gotBalance, err := strconv.ParseInt(res.Balances[0].Value, 10, 64)
+	require.NoError(mc.t, err)
+	return gotBalance
+}
+
+func assertBalanceAtTip(mc *mockChain, svr *Server, account string, wantBalance int64) {
+	mc.t.Helper()
+	gotBalance := accountBalanceAtTip(mc, svr, account)
+	if wantBalance != gotBalance {
+		mc.t.Fatalf("unexpected balance. want=%d got=%d", wantBalance, gotBalance)
+	}
+}
+
+func testPreprocessesAccounts(t *testing.T, db backenddb.DB) {
+	params := chaincfg.RegNetParams()
+	c := newMockChain(t, params)
+
+	pks := mustHex("76a91464098ee97e822d8b98e93608c4f0cdbb559146fa88ac")
+	addr := "RsHTkwUoDYKh3SJCj1FSoWbKy1P2DA6DHA5"
+
+	// disapprove disapproves the block.
+	disapprove := func(b *wire.MsgBlock) {
+		b.Header.VoteBits = 0
+	}
+
+	// Shorter function names to improve readability.
+	manglers := func(ms ...blockMangler) []blockMangler {
+		return ms
+	}
+	toAddr := func(v, nb int64) blockMangler {
+		return c.txToAddr(pks, v, nb)
+	}
+	fromAddr := func(v, nb int64) blockMangler {
+		return c.txFromAddr(pks, v, nb)
+	}
+
+	testCases := []struct {
+		name        string
+		manglers    []blockMangler
+		wantBalance int64
+	}{{
+		name:        "block before addr receives anything",
+		manglers:    manglers(),
+		wantBalance: 0,
+	}, {
+		name:        "single output added to address",
+		manglers:    manglers(toAddr(10, 1)),
+		wantBalance: 10,
+	}, {
+		name:        "second block without txs to addr",
+		manglers:    manglers(),
+		wantBalance: 10,
+	}, {
+		name:        "second block with single output added to address",
+		manglers:    manglers(toAddr(10, 1)),
+		wantBalance: 20,
+	}, {
+		name:        "multiple txs adding to addr",
+		manglers:    manglers(toAddr(10, 1), toAddr(10, 1)),
+		wantBalance: 40,
+	}, {
+		name:        "single tx sending multiple times to same addr",
+		manglers:    manglers(toAddr(10, 3)),
+		wantBalance: 70,
+	}, {
+		name:        "spend a sigle utxo in the block",
+		manglers:    manglers(fromAddr(10, 1)),
+		wantBalance: 60,
+	}, {
+		name:        "spend multiple utxos in the same tx",
+		manglers:    manglers(fromAddr(10, 2)),
+		wantBalance: 40,
+	}, {
+		name:        "spend multiple utxo in different txs",
+		manglers:    manglers(fromAddr(10, 1), fromAddr(10, 1)),
+		wantBalance: 20,
+	}, {
+		name:        "add and spend in the same block",
+		manglers:    manglers(toAddr(20, 1), fromAddr(10, 1)),
+		wantBalance: 30,
+	}, {
+		name:        "add to addr in a block that will be disapproved",
+		manglers:    manglers(toAddr(10, 1)),
+		wantBalance: 40,
+	}, {
+		name:        "disapprove the block that added to the addr",
+		manglers:    manglers(disapprove),
+		wantBalance: 30,
+	}, {
+		name:        "spend in a block that will be disapproved",
+		manglers:    manglers(fromAddr(10, 1)),
+		wantBalance: 20,
+	}, {
+		name:        "disapprove the block that spent from the addr",
+		manglers:    manglers(disapprove),
+		wantBalance: 30,
+	}, {
+		name:        "add and spend in a block that will be disapproved",
+		manglers:    manglers(toAddr(20, 1), fromAddr(10, 1)),
+		wantBalance: 40,
+	}, {
+		name:        "disapprove previous but also add in this block",
+		manglers:    manglers(disapprove, toAddr(20, 1)),
+		wantBalance: 50,
+	}, {
+		name:        "final block",
+		manglers:    manglers(),
+		wantBalance: 50,
+	}}
+
+	// Setup the chain that will be preprocessed.
+	for _, tc := range testCases {
+		c.extendTip(tc.manglers...)
+	}
+
+	// Create the server.
+	cfg := &ServerConfig{
+		ChainParams: params,
+		DBType:      dbTypePreconfigured,
+		c:           c,
+		db:          db,
+	}
+
+	ctxt, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+	svr, err := NewServer(ctxt, cfg)
+	require.NoError(t, err)
+
+	// Preprocess accounts.
+	err = svr.preProcessAccounts(testCtx(t))
+	require.NoError(t, err)
+
+	// Verify the historical balance at each block.
+	for i, tc := range testCases {
+		height := int64(i + 1)
+
+		// Verify the account balance by block height.
+		gotBalance := accountBalanceByHeight(t, svr, height, addr)
+		if gotBalance != tc.wantBalance {
+			t.Fatalf("'%s': unexpected balance. want=%d got=%d",
+				tc.name, tc.wantBalance, gotBalance)
+		}
+
+		// Verify the account balance by block hash.
+		bh := c.blocksByHeight[height]
+		gotBalance = accountBalanceByHash(t, svr, *bh, addr)
+		if gotBalance != tc.wantBalance {
+			t.Fatalf("'%s': unexpected balance by hash. want=%d got=%d",
+				tc.name, tc.wantBalance, gotBalance)
+		}
+	}
+
+	// Verify the balance at tip.
+	gotBalance := accountBalanceAtTip(c, svr, addr)
+	wantBalance := testCases[len(testCases)-1].wantBalance
+	if gotBalance != wantBalance {
+		t.Fatalf("unexpected balance at tip. want=%d got=%d",
+			wantBalance, gotBalance)
+	}
+}
+
+// TestPreprocessesAccounts ensures that pre-processing the blockchain yields
+// correct historical account balances.
+func TestPreprocessesAccounts(t *testing.T) {
+	testDbInstances(t, true, testPreprocessesAccounts)
+}
+
+func testPreprocessesGenesis(t *testing.T, db backenddb.DB) {
+	params := chaincfg.RegNetParams()
+	c := newMockChain(t, params)
+
+	// Initialize server.
+	cfg := &ServerConfig{
+		ChainParams: params,
+		DBType:      dbTypePreconfigured,
+		c:           c,
+		db:          db,
+	}
+	ctxt, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+	svr, err := NewServer(ctxt, cfg)
+	require.NoError(t, err)
+
+	// Preprocess.
+	err = svr.preProcessAccounts(testCtx(t))
+	require.NoError(t, err)
+}
+
+// TestPreprocessesGenesis tests that preprocessing the chain works when the
+// underlying chain is still at the genesis block.
+func TestPreprocessesGenesis(t *testing.T) {
+	testDbInstances(t, true, testPreprocessesGenesis)
+}
+
+func testPreprocessesAfterRestart(t *testing.T, db backenddb.DB) {
+	params := chaincfg.RegNetParams()
+	c := newMockChain(t, params)
+
+	pks := mustHex("76a91464098ee97e822d8b98e93608c4f0cdbb559146fa88ac")
+	addr := "RsHTkwUoDYKh3SJCj1FSoWbKy1P2DA6DHA5"
+
+	// Shorter function names to improve readability.
+	toAddr := func(v, nb int64) blockMangler {
+		return c.txToAddr(pks, v, nb)
+	}
+	fromAddr := func(v, nb int64) blockMangler {
+		return c.txFromAddr(pks, v, nb)
+	}
+
+	// Initialize server.
+	cfg := &ServerConfig{
+		ChainParams: params,
+		DBType:      dbTypePreconfigured,
+		c:           c,
+		db:          db,
+	}
+	ctxt, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+	svr, err := NewServer(ctxt, cfg)
+	require.NoError(t, err)
+
+	// Generate a chain that funds an account.
+	c.extendTip(toAddr(11, 1))
+	c.extendTip(fromAddr(5, 1))
+
+	// Preprocess.
+	err = svr.preProcessAccounts(testCtx(t))
+	require.NoError(t, err)
+
+	// Account balance should be correct.
+	assertBalanceAtTip(c, svr, addr, 6)
+
+	// Define the test cases.
+	testCases := []struct {
+		name         string
+		chainChanges func()
+		wantBalance  int64
+		wantError    bool
+	}{{
+		name:         "no changes before restarting",
+		chainChanges: func() {},
+		wantBalance:  6,
+	}, {
+		name: "chain extended",
+		chainChanges: func() {
+			c.extendTip(fromAddr(3, 1))
+			c.extendTip(toAddr(7, 1))
+		},
+		wantBalance: 10,
+	}, {
+		name: "chain reorged to same height",
+		chainChanges: func() {
+			c.rewindChain(1)
+			c.extendTip(toAddr(9, 1))
+		},
+		wantBalance: 12,
+	}, {
+		name: "chain reorged to higher height",
+		chainChanges: func() {
+			c.rewindChain(1)
+			c.extendTip(toAddr(19, 1))
+			c.extendTip(fromAddr(3, 1))
+		},
+		wantBalance: 19,
+	}, {
+		name: "chain reorged to lower height",
+		chainChanges: func() {
+			c.rewindChain(3)
+			c.extendTip(toAddr(5, 1))
+		},
+		wantError: true,
+	}, {
+		name: "reorged after having failed to process",
+		chainChanges: func() {
+			c.extendTip(toAddr(21, 1))
+			c.extendTip(fromAddr(5, 1))
+		},
+		wantBalance: 27,
+	}}
+
+	for _, tc := range testCases {
+		c.mtx.Lock()
+		tc.chainChanges()
+		c.mtx.Unlock()
+		t.Logf("case '%s'", tc.name)
+
+		// Preprocess again, simulating a restart.
+		err = svr.preProcessAccounts(testCtx(t))
+		if tc.wantError != (err != nil) {
+			t.Fatalf("unexpected error. want=%v got=%v",
+				tc.wantError, err != nil)
+		}
+		if err != nil {
+			// Do not assert balances when preprocessing should
+			// have errored.
+			continue
+		}
+
+		// Account balance should be the new value.
+		assertBalanceAtTip(c, svr, addr, tc.wantBalance)
+	}
+}
+
+// TestPreprocessesAfterRestart tests various cases of reprocessing the chain
+// after the server is restarted.
+func TestPreprocessesAfterRestart(t *testing.T) {
+	testDbInstances(t, false, testPreprocessesAfterRestart)
+}
+
+func testReorgDuringPreprocess(t *testing.T, db backenddb.DB) {
+	params := chaincfg.RegNetParams()
+	c := newMockChain(t, params)
+
+	pks := mustHex("76a91464098ee97e822d8b98e93608c4f0cdbb559146fa88ac")
+	addr := "RsHTkwUoDYKh3SJCj1FSoWbKy1P2DA6DHA5"
+
+	// Shorter function names to improve readability.
+	toAddr := func(v, nb int64) blockMangler {
+		return c.txToAddr(pks, v, nb)
+	}
+	fromAddr := func(v, nb int64) blockMangler {
+		return c.txFromAddr(pks, v, nb)
+	}
+
+	// Initialize server.
+	cfg := &ServerConfig{
+		ChainParams: params,
+		DBType:      dbTypePreconfigured,
+		c:           c,
+		db:          db,
+	}
+	ctxt, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+	svr, err := NewServer(ctxt, cfg)
+	require.NoError(t, err)
+
+	// Override concurrency to ensure multiple blocks are requested
+	// concurrently during preprocessing.
+	svr.concurrency = 4
+
+	// Generate a chain that funds an account.
+	c.extendTip(toAddr(11, 1))
+	c.extendTip(fromAddr(5, 1))
+
+	// Mark the point where we'll reorg to a new chain and also create
+	// enough blocks that parallel download of blocks will be saturated.
+	forkPoint := c.tipHash
+	c.extendTip(toAddr(7, 1))
+	for i := 0; i < svr.concurrency+1; i++ {
+		c.extendTip()
+	}
+	c.extendTip(fromAddr(3, 1))
+
+	// Hook into GetBlockHash. Before preprocess() fetches the forkPoint
+	// block, we'll cause a reorg.
+	c.getBlockHashHook = func(ctx context.Context, blockHeight int64) (*chainhash.Hash, error) {
+		bh, err := c.getBlockHash(ctx, blockHeight)
+		if err != nil {
+			return bh, err
+		}
+		if *bh != forkPoint {
+			// Not the reorg point.
+			return bh, err
+		}
+
+		// Give it enough time for the other goroutines that fetch
+		// blocks to fetch the blocks that will be reorged-out.
+		time.Sleep(time.Millisecond * 100)
+
+		// Cause the reorg.
+		nbBlocks := int(c.tipHeight - blockHeight)
+		c.rewindChain(nbBlocks)
+		c.extendTip(toAddr(17, 1))
+		for i := 0; i < svr.concurrency+1; i++ {
+			c.extendTip()
+		}
+		c.extendTip(fromAddr(5, 1))
+		return c.getBlockHash(ctx, blockHeight)
+	}
+
+	// Preprocess.
+	err = svr.preProcessAccounts(testCtx(t))
+	require.NoError(t, err)
+
+	// Account balance should be the reorged-in balance.
+	assertBalanceAtTip(c, svr, addr, 18)
+}
+
+// TestReorgDuringPreprocess tests that a reorg that happened during the
+// preprocessing of accounts is correctly handled.
+func TestReorgDuringPreprocess(t *testing.T) {
+	testDbInstances(t, true, testReorgDuringPreprocess)
+}
